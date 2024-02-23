50c63b64c0df3f25aaa6b4e49010d779c8b7ba05
bomjkee <e.smirnoov@yandex.ru>
2024-02-23 13:22:04 +0300

    test1

--- phonebook.cpp
+++ phonebook.cpp
@@ -1,5 +1,165 @@
 #include <iostream>
+#include <string>
+
+struct Node {
+    std::string name;
+    std::string phone;
+    Node* left;
+    Node* right;
+};
+
+Node* insert(Node* root, std::string name, std::string phone) {
+    if (root == nullptr) {
+        Node* newNode = new Node;
+        newNode->name = name;
+        newNode->phone = phone;
+        newNode->left = nullptr;
+        newNode->right = nullptr;
+        return newNode;
+    }
+    if (name < root->name) {
+        root->left = insert(root->left, name, phone);
+    } else if (name > root->name) {
+        root->right = insert(root->right, name, phone);
+    } else if (name == root->name) {
+        root->phone += ", " + phone;
+    }  
+    return root;
+}
+
+Node* min_node(Node* root) {
+    while (root->left != nullptr) {
+        root = root->left;
+    }
+    return root;
+}
+
+Node* name_search(Node* root, std::string name) {
+    if (root == nullptr || root->name == name) {
+        return root;
+    }
+    if (name < root->name) {
+        return name_search(root->left, name);
+    } 
+    else if (name > root->name) {
+        return name_search(root->right, name); 
+    }
+}
+
+Node* phone_search(Node* root, std::string phone) {
+    if (root == nullptr || root->phone == phone) {
+        return root;
+    }
+    Node* left_search = phone_search(root->left, phone);
+    Node* right_search = phone_search(root->right, phone);
+
+    return left_search ? left_search : right_search;
+}
+
+Node* remove(Node* root, std::string name) {
+    if (root == nullptr) {
+        return root;
+    }
+    if (name < root->name) {
+        root->left = remove(root->left, name);
+    } else if (name > root->name) {
+        root->right = remove(root->right, name);
+    } else {
+        if (root->left == nullptr) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        } else if (root->right == nullptr) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        } 
+        Node* temp = min_node(root->right);
+        root->name = temp->name;
+        root->phone = temp->phone;
+        root->right = remove(root->right, temp->name);
+    }
+    return root;
+}
+
+void Print(Node* root) {
+    if (root == NULL) {
+        return;
+    }
+    Print(root->left);
+    std::cout << root->name << ": " << root->phone << std::endl;
+    Print(root->right);
+}
 
 int main() {
+    Node* root = nullptr;
+    char choice;
+    do {
+        std::cout << "Phonebook" << std::endl;
+        std::cout << "1. Add a new contact" << std::endl;
+        std::cout << "2. Search contact to name" << std::endl;
+        std::cout << "3. Search contact to phone" << std::endl;
+        std::cout << "4. Remove a contact" << std::endl;
+        std::cout << "5. Show contact" << std::endl;
+        std::cout << "6. Exit" << std::endl;
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+        std::cout << "_______________________" << std::endl;
+        
+        switch(choice) {
+            case '1': {
+                std::string name, phone;
+                std::cout << "Enter name: ";
+                std::cin >> name;
+                std::cout << "Enter phone number: ";
+                std::cin >> phone;
+                root = insert(root, name, phone);
+                std::cout << "Done" << std::endl;
+                std::cout << "_______________________" << std::endl;
+                break;
+            }
+            case '2': {
+                std::string name;
+                std::cout << "Enter name to search: ";
+                std::cin >> name;
+                root = name_search(root, name);
+                if (root == nullptr) std::cout << "Try again" << std::endl;
+                else std::cout << "Phone: " << root->phone << std::endl;
+                std::cout << "_______________________" << std::endl;
+                break;
+            }
+            case '3': {
+                std::string phone;
+                std::cout << "Enter phone to search: ";
+                std::cin >> phone;
+                root = name_search(root, phone);
+                if (root == nullptr) std::cout << "Try again" << std::endl;
+                else std::cout << "Name: " << root->phone << std::endl;
+                std::cout << "_______________________" << std::endl;
+                break;
+            }
+            case '4': {
+                std::string name;
+                std::cout << "Enter name to remove: ";
+                std::cin >> name;
+                root = remove(root, name);
+                std::cout << "Done" << std::endl;
+                std::cout << "_______________________" << std::endl;
+                break;
+            }
+            case '5': {
+                std::cout << "Contacts:" << std::endl;
+                Print(root);
+                std::cout << "_______________________" << std::endl;
+                break;
+            }
+            case '6':
+                std::cout << "Exiting..." << std::endl;
+                std::cout << "Have a nice day";
+                break;
+            default:
+                std::cout << "Ivalid choice. Retry please" << std::endl;
+        }
+    } while (choice != '6');
     return 0;
 }
---
+++ treecut.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+struct Node {
+    int min_key, max_key, value;
+    Node* left = nullptr;
+    Node* right = nullptr;
+};
+
+Node* new_node(int min_key, int max_key) {
+    Node* newNode = new Node;
+    newNode->min_key = min_key;
+    newNode->max_key = max_key;
+    newNode->left = newNode->right = nullptr;
+    if (newNode->min_key == newNode->max_key) newNode->value = 0;
+    return newNode;
+}
+
+Node* build_tree(Node* node, int min_key, int max_key) {
+    if (node == nullptr) {
+        node = new_node(min_key, max_key);
+        build_tree(node, min_key, max_key);
+    }
+    else {
+        if (node->max_key - node->min_key == 1) {
+            node->value = node->min_key + node->max_key; 
+            return node;
+        }
+        else {
+            int past = (min_key + max_key) / 2;
+            if (past == min_key) return nullptr;
+            else node->left = build_tree(new_node(min_key, past), min_key, past);
+            if (past == max_key)  return nullptr;
+            else node->right = build_tree(new_node(past, max_key), past, max_key);
+            node->value = (node->left ? node->left->value - node->left->min_key : 0) + (node->right ? node->right->value - node->right->min_key : 0);
+        }
+    }
+    return node;
+}
+
+void print_tree_util (Node* root, int space) {
+    int count_space = 7;
+    if (root == nullptr) return;
+    space += count_space;
+    print_tree_util(root->right, space);
+    std::cout << std::endl;
+    for(int i = count_space; i < space; i++) std::cout << ' ';
+    std::cout << '(' << root->min_key << ", " << root->max_key << ')' << std::endl;
+    print_tree_util(root->left, space);
+}
+
+void print_tree(Node* root) {    
+    print_tree_util(root, 0);
+}
+
+int search_util(Node* root, int value, int &count) {
+    if (root == nullptr) return count;
+    if (root->min_key > value) return count;
+    if (root->max_key < value) return count;
+    if (root->min_key <= value && root->max_key >= value) count++;
+    search_util(root->left, value, count);
+    search_util(root->right, value, count);
+    return count;
+    }
+
+int search(Node* root, int value) {
+    int count = 0;
+    search_util(root, value, count);
+    return count;
+}
+
+int summ (Node* root, int first, int last) {
+    if (root == nullptr) return 0;
+    if (first > root->max_key || last < root->min_key) return 0;
+    if (first <= root->min_key && last >= root->max_key) return root->value;
+    return summ(root->left, first, last) + summ(root->right, first, last);
+}
+
+int main() {
+    Node* root = nullptr;
+    int value, first, last;
+    int f, l;
+    std::cout << "Set a segment of the tree: ";
+    std::cin >> f >> l; 
+    std::cout << "Running..." << std::endl;
+    std::cout << "___________________________________________";
+    root = build_tree(root, f, l);
+    print_tree(root);
+    std::cout << "____________________________________________" << std::endl;
+    std::cout << "Enter the value: ";
+    std::cin >> value;
+    std::cout << "Count value in range: " << search(root, value) << std::endl;
+    std::cout << "____________________________________________" << std::endl;
+    std::cout << "Please enter the beginning and end of the segment: ";
+    std::cin >> first >> last;
+    std::cout << "The summ of the segment: " << summ(root, first, last) << std::endl;
+    std::cout << "___________________________________________";
+    return 0;
+}
