1c7893fe661c90d1d365a43e7fc1a11150d31c07
bomjkee <e.smirnoov@yandex.ru>
2024-02-25 19:31:52 +0300

    finally

--- treecut.cpp
+++ treecut.cpp
@@ -4,61 +4,48 @@
 определенном интервале*/
 
 #include <iostream>
+#include <string>
 
-struct Node {
+struct Tree {
     int min_key, max_key;
-    int value;
-    Node* left = nullptr;
-    Node* right = nullptr;
+    int sum;
+    Tree *left, *right;
+    Tree(int s, int l) : min_key(s), max_key(l), sum(0), left(nullptr), right(nullptr) {}
 };
 
-Node* new_node(int min_key, int max_key) {
-    Node* newNode = new Node;
-    newNode->min_key = min_key;
-    newNode->max_key = max_key;
-    newNode->left = newNode->right = nullptr;
-    if (newNode->min_key == newNode->max_key) newNode->value = 0;
-    return newNode;
-}
-
-Node* build_tree(Node* node, int min_key, int max_key) {
-    if (node == nullptr) {
-        node = new_node(min_key, max_key);
-        build_tree(node, min_key, max_key);
-    }
-    else {
-        if (node->max_key - node->min_key == 1) {
-            node->value = node->max_key; 
-            return node;
+Tree* build_tree(int min_key, int max_key) {
+    if (min_key > max_key) return nullptr;
+    Tree* node = new Tree(min_key, max_key); 
+    if (min_key == max_key) node->sum = node->min_key;
+    else if (max_key - min_key == 1) {
+        node->left = new Tree(min_key, min_key);
+        node->left->sum = node->min_key;
+        node->right = new Tree(max_key, max_key);
+        node->right->sum = node->max_key;
+        } else {
+            int past = min_key + (max_key - min_key) / 2;
+            node->left = build_tree(min_key, past);
+            node->right = build_tree(past + 1, max_key);
+            node->sum = (node->left ? node->left->sum : 0) + (node->right ? node->right->sum : 0);
         }
-        else {
-            int past = (min_key + max_key) / 2;
-            if (past == min_key) return nullptr;
-            else node->left = build_tree(new_node(min_key, past), min_key, past);
-            if (past == max_key)  return nullptr;
-            else node->right = build_tree(new_node(past, max_key), past, max_key);
-            node->value = (node->left ? node->left->value : 0) + (node->right ? node->right->value : 0);
-        }
-    }
     return node;
 }
 
-void print_tree_util (Node* root, int space) {
-    int count_space = 7;
-    if (root == nullptr) return;
-    space += count_space;
-    print_tree_util(root->right, space);
-    std::cout << std::endl;
-    for(int i = count_space; i < space; i++) std::cout << ' ';
-    std::cout << '[' << root->min_key << ", " << root->max_key << ']' << std::endl;
-    print_tree_util(root->left, space);
+void print_tree_util(Tree* node, const std::string &indent = "", bool isLeft = true) {
+    if(node == nullptr) return;
+    if(node->right) print_tree_util(node->right, indent + (isLeft ? "|   " : "    "), false);
+    std::cout << indent;
+    if(isLeft) std::cout << "\\---";
+    else std::cout << "/---";
+    std::cout << "[" << node->min_key << "," << node->max_key << "]" << std::endl;
+    if(node->left) print_tree_util(node->left, indent + (isLeft ? "    " : "|   "), true);
 }
 
-void print_tree(Node* root) {    
-    print_tree_util(root, 0);
+void print_tree(Tree* root) {
+    print_tree_util(root, "", false);
 }
 
-int search_util(Node* root, int value, int &count) {
+int search_util(Tree* root, int value, int& count) {
     if (root == nullptr) return count;
     if (root->min_key > value) return count;
     if (root->max_key < value) return count;
@@ -66,30 +53,31 @@ int search_util(Node* root, int value, int &count) {
     search_util(root->left, value, count);
     search_util(root->right, value, count);
     return count;
-    }
+}
 
-int search(Node* root, int value) {
+int search(Tree* root, int value) {
     int count = 0;
     search_util(root, value, count);
     return count;
 }
 
-int summ (Node* root, int first, int last) {
-    if (root == nullptr) return 0;
-    if (first > root->max_key || last < root->min_key) return 0;
-    if (first <= root->min_key && last >= root->max_key) return root->value;
-    return summ(root->left, first, last) + summ(root->right, first, last);
+int tree_summ(Tree* node, int L, int R) {
+    if (node == nullptr || L > node->max_key || R < node->min_key) return 0;
+    if (node->min_key == node->max_key) return node->min_key;
+    if (L <= node->min_key && R >= node->max_key) {
+        return (node->max_key - node->min_key + 1) * (node->min_key + node->max_key) / 2;
+    }
+    return tree_summ(node->left, L, R) + tree_summ(node->right, L, R);
 }
 
+
 int main() {
-    Node* root = nullptr;
     int value, first, last;
     int f, l;
     std::cout << "Set a segment of the tree: ";
-    std::cin >> f >> l; 
+    std::cin >> f >> l;
     std::cout << "Running..." << std::endl;
-    std::cout << "___________________________________________";
-    root = build_tree(root, f, l);
+    Tree* root = build_tree(f, l);
     print_tree(root);
     std::cout << "____________________________________________" << std::endl;
     std::cout << "Enter the value: ";
@@ -98,7 +86,7 @@ int main() {
     std::cout << "____________________________________________" << std::endl;
     std::cout << "Please enter the beginning and end of the segment: ";
     std::cin >> first >> last;
-    std::cout << "The summ of the segment: " << summ(root, first, last) + first << std::endl;
+    std::cout << "The summ of the segment " << '[' << first << ", " << last << ']' << ": " <<tree_summ(root, first, last) << std::endl;
     std::cout << "___________________________________________";
     return 0;
 }
